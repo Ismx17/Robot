d35a6848619c6f422bffed61f4b72b7f515dbb8f
Ismx17 <irosros933@gmail.com>
2024-11-27 09:05:59 +0100

    Add validation and movement methods for Robot and Zona
    Enhanced `Zona` with validation checks for dimensions and added coordinate-based checks. Introduced `Robot` constructors with parameters and implemented movement methods based on the current orientation.
    
    Signed-off-by: Ismx17 <irosros933@gmail.com>

--- src/main/java/org/iesalandalus/programacion/robot/modelo/Robot.java
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Robot.java
@@ -1,22 +1,127 @@
 package org.iesalandalus.programacion.robot.modelo;
 
+import javax.naming.OperationNotSupportedException;
+import java.util.Objects;
+
 public class Robot {
+
+    private Zona zona;
+    private Orientacion orientacion;
+    private Coordenada coordenada;
 
     public Robot() {
     }
 
+    public Coordenada getPosicion() {
+        return coordenada;
+    }
+
+
     public Robot(Zona zona) {
+        this.zona = zona;
     }
 
     public Robot(Zona zona, Orientacion orientacion) {
+        this.zona = zona;
+        this.orientacion = orientacion;
     }
 
     public Robot(Zona zona, Orientacion orientacion, Coordenada coordenada) {
+        this.zona = zona;
+        this.orientacion = orientacion;
+        this.coordenada = coordenada;
     }
 
     public Robot(Robot robot) {
+        this.zona = robot.zona;
+        this.orientacion = robot.orientacion;
+        this.coordenada = robot.coordenada;
     }
 
-    public void getZona() {
+    public Zona getZona() {
+        return this.zona;
+    }
+
+    private Zona setZona(Zona zona) {
+        return zona;
+    }
+
+    public Orientacion getOrientacion() {
+        return this.orientacion;
+    }
+
+    private Orientacion setOrientacion(Orientacion orientacion) {
+        return orientacion;
+    }
+
+    public Coordenada getCoordenada() {
+        return this.coordenada;
+    }
+
+    private Coordenada setCoordenada(Coordenada coordenada) {
+        return coordenada;
+    }
+
+    public void avanzar() throws OperationNotSupportedException {
+        int nuevaX = coordenada.x();
+        int nuevaY = coordenada.y();
+        switch (orientacion) {
+            case NORTE -> nuevaY++;
+            case NORESTE -> {nuevaX++; nuevaY++;}
+            case ESTE -> nuevaX++;
+            case SURESTE -> {nuevaY--; nuevaX++;}
+            case SUR -> nuevaY--;
+            case SUROESTE -> {nuevaX--; nuevaY--;}
+            case OESTE -> nuevaX--;
+            case NOROESTE -> {nuevaX--; nuevaY++;}
+        }
+        try {
+            setCoordenada(new Coordenada(nuevaX, nuevaY));
+        } catch (IllegalArgumentException e) {
+            throw new OperationNotSupportedException("No se puede avanzar, ya que se sale de la zona.");
+        }
+    }
+
+    public void girarALaIzquierda() {
+        orientacion = switch (orientacion) {
+            case NORTE -> Orientacion.NOROESTE;
+            case NORESTE -> Orientacion.NORTE;
+            case ESTE -> Orientacion.NORESTE;
+            case SURESTE -> Orientacion.ESTE;
+            case SUR -> Orientacion.SURESTE;
+            case SUROESTE -> Orientacion.SUR;
+            case OESTE -> Orientacion.SUROESTE;
+            case NOROESTE -> Orientacion.OESTE;
+        };
+    }
+
+    public void girarALaDerecha() {
+        orientacion = switch (orientacion) {
+            case NORTE -> Orientacion.NORESTE;
+            case NORESTE -> Orientacion.ESTE;
+            case ESTE -> Orientacion.SURESTE;
+            case SURESTE -> Orientacion.SUR;
+            case SUR -> Orientacion.SUROESTE;
+            case SUROESTE -> Orientacion.OESTE;
+            case OESTE -> Orientacion.NOROESTE;
+            case NOROESTE -> Orientacion.NORTE;
+        };
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Robot robot)) return false;
+        return Objects.equals(zona, robot.zona) && orientacion == robot.orientacion && Objects.equals(coordenada, robot.coordenada);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(zona, orientacion, coordenada);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Robot[zona=%s, orientacion=%s, coordenada=%s]", this.zona, this.orientacion, this.coordenada);
     }
 }
--- src/main/java/org/iesalandalus/programacion/robot/modelo/Zona.java
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Zona.java
@@ -1,36 +1,68 @@
 package org.iesalandalus.programacion.robot.modelo;
 
-public record Zona() {
+public class Zona {
     public static final int ANCHO_MINIMO = 10;
     public static final int ANCHO_MAXIMO = 100;
     public static final int ALTO_MINIMO = 10;
     public static final int ALTO_MAXIMO = 100;
 
-    private static int ancho;
-    private static int alto;
+    private int ancho;
+    private int alto;
 
-    public Zona(int x, int y) {
+    public Zona(int ancho, int alto) {
+        this();
         this.ancho = ancho;
         this.alto = alto;
     }
 
     public Zona() {
-        ancho = ANCHO_MAXIMO;
-        alto = ALTO_MAXIMO;
+        this.ancho = ANCHO_MAXIMO;
+        this.alto = ALTO_MAXIMO;
     }
 
     private int validarAncho(int ancho) {
-        return ancho;
+        if (ancho >= ANCHO_MINIMO && ancho <= ANCHO_MAXIMO) {
+            return ancho;
+        } else {
+            throw new IllegalArgumentException("Ancho fuera de los límites permitidos");
+        }
     }
 
     private int validarAlto(int alto) {
-        return alto;
+        if (alto >= ALTO_MINIMO && alto <= ALTO_MAXIMO) {
+            return alto;
+        } else {
+            throw new IllegalArgumentException("Alto fuera de los límites permitidos");
+        }
     }
 
-    public static void getCentro(Coordenada coordenada) {
+    public static Coordenada getCentro(int ancho, int alto) {
+        int centroX = (ancho / 2);
+        int centroY = (alto / 2);
+        return new Coordenada(centroX, centroY);
     }
 
     public boolean pertenece(Coordenada coordenada) {
-        return
+        if (coordenada.x() >= 10 & coordenada.x() <= 100 & coordenada.y() >= 10 & coordenada.y() <= 100) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean perteneceX(int x) {
+        if (x >= 10 && x <= 100) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean perteneceY(int y) {
+        if (y >= 10 && y <= 100) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
